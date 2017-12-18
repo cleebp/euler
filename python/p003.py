47e2e032b647461c3566354e2653b81e262259ac
Brian Clee <bpclee2011@gmail.com>
2017-12-18 13:27:46 -0500

    p003 solution added

--- python/p003.py
+++ python/p003.py
@@ -6,3 +6,18 @@ https://projecteuler.net/problem=3
 The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?
 """
+
+def max_factors(num):
+    i = 2
+    # largest prime factor will never be larger than the square root of num
+    while i * i < num:
+        while num % i == 0:
+            num = num / i
+        i = i + 1
+    return num
+
+def main():
+    max_factor = max_factors(600851475143)
+    print("p003 answer:",max_factor)
+
+main()
