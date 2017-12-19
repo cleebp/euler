91f98d0e6c7d7e91063349f35c0c3ada5be4a416
Brian Clee <bpclee2011@gmail.com>
2017-12-19 13:40:51 -0500

    p006 solution added

--- python/p006.py
+++ python/p006.py
@@ -12,3 +12,19 @@ Hence the difference between the sum of the squares of the first ten natural num
 3025 − 385 = 2640.
 Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 """
+
+def squares():
+    sum_squares = 0
+    sums = 0
+    for i in range(1,101):
+        sum_squares = sum_squares + i**2
+        sums = sums + i
+    sums = sums**2
+    result = sums - sum_squares
+    return result
+    
+def main():
+    result = squares()
+    print("p006 answer:",result)
+
+main()
